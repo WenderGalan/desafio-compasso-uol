1d9d5c7e479d36803987a939f95d6389f4e3cc62
Wender Galan <wendergalan2014@hotmail.com>
2021-02-17 19:49:09 -0400

    Commit inicial, inicializando o projeto.

---
+++ src/test/java/io/github/wendergalan/desafiocompassouol/DesafioCompassoUolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wendergalan.desafiocompassouol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCompassoUolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
