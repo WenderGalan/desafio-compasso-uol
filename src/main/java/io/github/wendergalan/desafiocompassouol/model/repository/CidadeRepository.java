675de10efc7013d97ba15a2af18aaac0ccbb1678
Wender Galan <wendergalan2014@hotmail.com>
2021-02-17 20:43:57 -0400

    Criando os repositories do dominio.

---
+++ src/main/java/io/github/wendergalan/desafiocompassouol/model/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package io.github.wendergalan.desafiocompassouol.model.repository;
+
+import io.github.wendergalan.desafiocompassouol.model.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+}
---
+++ src/main/java/io/github/wendergalan/desafiocompassouol/model/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package io.github.wendergalan.desafiocompassouol.model.repository;
+
+import io.github.wendergalan.desafiocompassouol.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
