061892940d9750d49a5fb1c7842ba78a482ea407
Wender Galan <wendergalan2014@hotmail.com>
2021-02-20 00:27:56 -0400

    Adicionando config para deploy no heroku.

--- src/main/java/io/github/wendergalan/desafiocompassouol/DesafioCompassoUolApplication.java
+++ src/main/java/io/github/wendergalan/desafiocompassouol/DesafioCompassoUolApplication.java
@@ -2,12 +2,18 @@ package io.github.wendergalan.desafiocompassouol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DesafioCompassoUolApplication {
+public class DesafioCompassoUolApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioCompassoUolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioCompassoUolApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DesafioCompassoUolApplication.class);
+    }
 }
